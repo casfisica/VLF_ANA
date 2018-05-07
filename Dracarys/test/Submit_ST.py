b0a852cdb14d79f3d78b1e577ee79af7f67ac8ac
Camilo Salazar <camilo.salazar@cern.ch>
2018-05-07 10:34:54 +0200

    Submit_* add

---
+++ Dracarys/test/Submit_ST.py
@@ -0,0 +1,26 @@
+import commands as cmd
+import time
+
+ListOfSamples=['/ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1/MINIAODSIM',
+               '/ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1/MINIAODSIM',
+               '/ST_t-channel_top_4f_inclusiveDecays_TuneCUETP8M2T4_13TeV-powhegV2-madspin/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM',
+               '/ST_t-channel_antitop_4f_inclusiveDecays_TuneCUETP8M2T4_13TeV-powhegV2-madspin/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM',
+               '/ST_s-channel_4f_leptonDecays_13TeV-amcatnlo-pythia8_TuneCUETP8M1/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM']
+
+ITime=time.localtime()
+StringITime=str(ITime.tm_year)+'_'+str(ITime.tm_mon)+'_'+str(ITime.tm_mday)+'_'+str(ITime.tm_hour)+'_'+str(ITime.tm_min)
+print "Submitting Jobs "+StringITime
+
+WorkingFolder='CrabConfigs_'+StringITime
+cmd.getoutput('mkdir '+WorkingFolder)
+
+for i in ListOfSamples:
+    UsefulString=i.split('/')[1]+"_"+i.split('/')[2]
+    cmd.getoutput('cp Crab_Template.py '+WorkingFolder+'/Crab_'+UsefulString+'.py')
+    cmd.getoutput('sed -i -- "s/OUTPUTDIR/ST/g" '+WorkingFolder+'/Crab_'+UsefulString+'.py')
+    cmd.getoutput('sed -i -- "s/TASK/'+UsefulString.split('Tune')[0]+i.split('/')[2].split("_")[-1]+'_'+StringITime+'/g" '+WorkingFolder+'/Crab_'+UsefulString+'.py')
+    cmd.getoutput('sed -i -- "s#DATASAMPLE#'+i+'#g" '+WorkingFolder+'/Crab_'+UsefulString+'.py')
+    print "Job being submitted: "+UsefulString
+    CrabOutput=cmd.getoutput('crab submit -c '+WorkingFolder+'/Crab_'+UsefulString+'.py')
+    print CrabOutput
+    print "---------------------------------------------------------------------------------------"
---
+++ Dracarys/test/Submit_WW.py
@@ -0,0 +1,22 @@
+import commands as cmd
+import time
+
+ListOfSamples=['/WWTo2L2Nu_13TeV-powheg/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM']
+
+ITime=time.localtime()
+StringITime=str(ITime.tm_year)+'_'+str(ITime.tm_mon)+'_'+str(ITime.tm_mday)+'_'+str(ITime.tm_hour)+'_'+str(ITime.tm_min)
+print "Submitting Jobs "+StringITime
+
+WorkingFolder='CrabConfigs_'+StringITime
+cmd.getoutput('mkdir '+WorkingFolder)
+
+for i in ListOfSamples:
+    UsefulString=i.split('/')[1]+"_"+i.split('/')[2]
+    cmd.getoutput('cp Crab_Template.py '+WorkingFolder+'/Crab_'+UsefulString+'.py')
+    cmd.getoutput('sed -i -- "s/OUTPUTDIR/WW/g" '+WorkingFolder+'/Crab_'+UsefulString+'.py')
+    cmd.getoutput('sed -i -- "s/TASK/'+UsefulString.split('Tune')[0]+i.split('/')[2].split("_")[-1]+'_'+StringITime+'/g" '+WorkingFolder+'/Crab_'+UsefulString+'.py')
+    cmd.getoutput('sed -i -- "s#DATASAMPLE#'+i+'#g" '+WorkingFolder+'/Crab_'+UsefulString+'.py')
+    print "Job being submitted: "+UsefulString
+    CrabOutput=cmd.getoutput('crab submit -c '+WorkingFolder+'/Crab_'+UsefulString+'.py')
+    print CrabOutput
+    print "---------------------------------------------------------------------------------------"
