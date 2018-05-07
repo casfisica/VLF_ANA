5ef6898f5445f842f2d22b54882ec25d883fb885
Camilo Salazar <camilo.salazar@cern.ch>
2018-05-07 10:41:06 +0200

    changes

--- Dracarys/python/ConfFile_cfg.py
+++ Dracarys/python/ConfFile_cfg.py
@@ -53,7 +53,7 @@ process.demo = cms.EDAnalyzer('Dracarys',
                               #Activate debug option
                               debug = cms.bool(False),
                               #Trigger variables
-                              isTrigger = cms.bool(False),
+                              isTrigger = cms.bool(True),
                               isTriggerToo = cms.bool(False),
                               TriggerPath1 = cms.string("HLT_PFMET110_PFMHT110_IDTight"),
                               TriggerPath2 = cms.string("HLT_DoubleMu3_PFMET50"),
